656456038245a1a7d8fab2be896c3f8bc8143504
Theophelous Mudhleyo <theophelousmudhleyo@gmail.com>
2026-05-28 16:24:13 +0530

    chore: upgrade Next.js to v15.3.6 and cleanup assets
    - Update Next.js to 15.3.6
    - Remove unused 404 and 500 error pages
    - Remove legacy and redundant image assets

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-  return (
-    <div style={{ padding: '100px 0', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <a href="/" style={{ color: '#3b82f6', textDecoration: 'underline' }}>Go back home</a>
-    </div>
-  );
-}
